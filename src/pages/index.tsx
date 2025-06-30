6743c044c1651301e19ea3b8d99b44924c8406fe
Andy <andychouforwk.gmail.com>
2025-06-30 10:35:03 +0800

    create loginPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,22 @@
-import Layout from "../components/Layout";
+
 
 
 export default function Home() {
   return (
-    <div>
-      <Layout>
-        CRM
-      </Layout>
+    <div className="flex justify-center">
+        <div className=" mt-[40vh]">
+          <div>
+            <label className="  bg-amber-200" htmlFor="username">UserName</label>
+            <input id="username" type="text" className="border-1" />
+          </div>
+          <div>
+            <label className="  bg-amber-200" htmlFor="password">Password</label>
+            <input id="password" type="password" className="border-1"/>
+          </div>
+          <div>
+            
+          </div>
+        </div>
     </div>
   );
 }
